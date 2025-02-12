299d47ddf6da7aded03b7f7cc6a04be24542e770
adriangmweb <adriangonzalezmesa@gmail.com>
2025-02-12 10:03:19 +0000

    increase parameters

--- v2.py
+++ v2.py
@@ -3,13 +3,16 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 500
 learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
+n_embed = 384
+n_heads = 6
+n_layer = 6
 dropout = 0.2 # Randomly drop out 20% of the neurons to prevent overfitting
 # ------------
 
